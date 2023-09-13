b79daed5e5233961829cc26ee8c8273e8f063cd0
charmie11 <57680262+charmie11@users.noreply.github.com>
2023-09-13 22:10:35 +0900

    修正
    ノイズ関連項目削除
    サンプリング回数関連項目追加

--- test/main.py
+++ test/main.py
@@ -16,15 +16,16 @@ def main():
     Vs = [1.0, 10.0]
     Rs = [1.0, 5.0]
     Cs = [1.0, 5.0]
-    Taus = [Rs[0]*Cs[0], Rs[1]*Cs[1]]
+    nums = [10, 300]
 
     V_0 = np.mean(Vs)
     R_0 = 2.0
     C_0 = 3.0
+    num_0 = 100
     tau_0 = C_0 * R_0
     tau_max = Cs[1] * Rs[1]
-    t = np.linspace(-tau_max, 6*tau_max, 2000)
-    v_c = V_0 * (1.0 - np.exp(-t/tau_0))
+    t = np.linspace(0.0, 6 * tau_0, num_0)
+    v_c = V_0 * (1.0 - np.exp(-t / tau_0))
     v_c[np.where(t < 0.0)] = 0.0
     v = V_0 * np.ones(t.shape)
     v[np.where(t < 0)] = 0.0
@@ -37,9 +38,9 @@ def main():
     plot_options = dict(plot_width=512, plot_height=300, x_axis_label="Time [s]")
 
     # plot V
-    plot_v = figure(x_range=(-0.1*tau_max, 6*tau_max),
+    plot_v = figure(x_range=(-0.1 * tau_max, 6 * tau_max),
                     y_axis_label="Volt [V]",
-                    y_range=(-0.3*Vs[0], 1.1*Vs[1]),
+                    y_range=(-0.3 * Vs[0], 1.1 * Vs[1]),
                     **plot_options)
     plot_v.title.text = "電源電圧とコンデンサの電圧"
     plot_v.title.align = "center"
@@ -62,7 +63,7 @@ def main():
     # plot I
     plot_i = figure(x_range=plot_v.x_range,
                     y_axis_label="Current [A]",
-                    y_range=(-1.1*Vs[1]/Rs[0], 1.1*Vs[1]/Rs[0]),
+                    y_range=(-1.1 * Vs[1] / Rs[0], 1.1 * Vs[1] / Rs[0]),
                     **plot_options)
     plot_i.title.text = "回路に流れる電流"
     plot_i.title.align = "center"
@@ -84,42 +85,37 @@ def main():
     """ widgets """
     # sliders
     amplitude = Slider(value=V_0, start=Vs[0], end=Vs[1], step=0.01, title=u"電源電圧 [V]", format="0.00")
-    resistance = Slider(value=R_0, start=Rs[0], end=Rs[1], step=0.01, title=u'抵抗値 (内部抵抗含む) [Ω]', format="0.00")
+    resistance = Slider(value=R_0, start=Rs[0], end=Rs[1], step=0.01, title=u'抵抗値 [Ω]', format="0.00")
     capacitance = Slider(value=C_0, start=Cs[0], end=Cs[1], step=0.01, title=u"静電容量 [F]", format="0.00")
+    num = Slider(value=num_0, start=nums[0], end=nums[1], step=10, title=u"計測回数/周期[回]")
 
     # buttons
     mode_charge = RadioButtonGroup(labels=[u"充電", u"放電"], active=0)
     mode_charge.button_type = "success"
-    mode_noise = RadioButtonGroup(labels=["ノイズなし", "ノイズあり"], active=0)
-    mode_noise.button_type = "success"
 
     code_sliders = """
         // data from GUI
-        var mode_n = mode_noise.active;
         var mode_c = mode_charge.active;
         const V = amp.value;
         //const V = Math.round(10.0*amp.value)/10.0;
         const R = res.value;
         const C = cap.value;
-        
+
         // data of voltage
         var data_v = observation_v.data;
         data_v['t'] = [];
         data_v['v_c'] = [];
-        
+
         // data of current
         var data_i = observation_i.data;
         data_i['t'] = [];
         data_i['i_c'] = [];
-        
+
         // data of time
         const tau = R * C;
-        const tau_max = 25.0;
-//        const t_max = 6*tau_max;
-        const t_max = 6*tau_max;
-        const t_step = 6 * tau_max / 2000;
-//        const t_step = tau/100;
-        var t_i = -0.2*tau_max;
+        const t_max = 6*tau;
+        const t_step = t_max/num.value;
+        var t_i = 0.0*t_max;
 
         // main loop
         while(t_i < t_max){
@@ -134,17 +130,14 @@ def main():
             else{  // Discharge
                 data_v['v_c'].push(Math.min(V, V * (Math.exp(-t_i/tau))));
             }
-            if(mode_n == 1){
-                data_v['v_c'][data_v['v_c'].length - 1] += 0.02 * Math.random();
-            }
-            
+
             // update I_c
             var V_c = data_v['v_c'][data_v['v_c'].length - 1];
             if(mode_c == 0){  // Charge
-                data_i['i_c'].push(t_i < 0.0 ? 0.0 : (V - V_c)/R);
+                data_i['i_c'].push((V - V_c)/R);
             }
             else{  // Discharge
-                data_i['i_c'].push(t_i < 0.0 ? 0.0 : -V_c/R);
+                data_i['i_c'].push(-V_c/R);
             }
 
             // update t_i
@@ -154,13 +147,12 @@ def main():
         observation_i.change.emit();
     """
     callback = CustomJS(args=dict(observation_v=observation_v, observation_i=observation_i,
-                                  # time_range=time_range,
-                                  amp=amplitude, res=resistance, cap=capacitance,
-                                  mode_charge=mode_charge, mode_noise=mode_noise),
+                                  amp=amplitude, res=resistance, cap=capacitance, num=num,
+                                  mode_charge=mode_charge),
                         code=code_sliders)
-    for w in [amplitude, resistance, capacitance]:
+    for w in [amplitude, resistance, capacitance, num]:
         w.js_on_change('value', callback)
-    for m in [mode_charge, mode_noise]:
+    for m in [mode_charge]:
         m.js_on_change('active', callback)
 
     # download button
@@ -174,15 +166,17 @@ def main():
     """ layout """
     empty_space = Spacer(height=20)
     title_parameters = Paragraph(text=u"CR回路の各種パラメータ設定用スライダー", align='center')
-    par_parameters = Paragraph(text=u"以下のスライダーでパラメータの数値を変更でき，ページ右側のグラフが動的に変化します．")
-    parameters = column(title_parameters, par_parameters, amplitude, resistance, capacitance)
+    par_parameters = Paragraph(
+        text=u"以下のスライダーでパラメータの数値を変更でき，ページ右側のグラフが動的に変化します．")
+    parameters = column(title_parameters, par_parameters, amplitude, resistance, capacitance, num)
 
     title_modes = Paragraph(text=u"動作変更ボタン", align='center')
     par_modes = Paragraph(text=u"以下のボタンで動作モードを変更でき，ページ右側のグラフが動的に変化します．")
-    modes = column(title_modes, par_modes, mode_charge, mode_noise)
+    modes = column(title_modes, par_modes, mode_charge)
 
     title_download = Paragraph(text=u"波形データダウンロードボタン", align='center')
-    par_download = Paragraph(text=u"以下のボタンを押すと，右上のグラフのデータ(時刻，電源電圧，コンデンサの電圧)がcsvファイルとして保存できます．")
+    par_download = Paragraph(
+        text=u"以下のボタンを押すと，右上のグラフのデータ(時刻，電源電圧，コンデンサの電圧)がcsvファイルとして保存できます．")
     download = column(title_download, par_download, button_download)
 
     plots = column(plot_v, empty_space, plot_i)
